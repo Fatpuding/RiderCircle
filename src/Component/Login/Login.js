f5443eb7b9e4ef8e09e2ea24632ac86c4fd3075e
Your Name <you@example.com>
2021-06-24 17:32:31 +0800

    update

---
+++ src/Component/Login/Login.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+class Login extends Component {
+
+    render() {
+        return (
+            <div>
+                <span>login</span>
+            </div>
+        );
+    }
+}
+
+export default Login;
